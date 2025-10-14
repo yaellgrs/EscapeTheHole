3359b3c22a07939fedaa7e496fbe5b6312bead22
yaellgrs <yaellegris09@gmail.com>
2025-10-15 00:20:42 +0200

    Implement simple movement and rotation for player

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
+    public float speed = 5f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody rb;
+    private Vector3 move;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        move = new Vector3(horizontal, 0, vertical).normalized;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (move.magnitude > 0.01f)
+        {
+            rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+
+            // Rotation progressive (smooth)
+            Quaternion targetRot = Quaternion.LookRotation(move);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRot, rotationSpeed * Time.fixedDeltaTime));
+        }
     }
 }
+
